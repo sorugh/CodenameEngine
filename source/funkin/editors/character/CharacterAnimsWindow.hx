63805b806b81ae9d0dff6ccc0aa78a7fa7795232
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-04-16 17:38:35 -0700

    wip for display windows

--- source/funkin/editors/character/CharacterAnimsWindow.hx
+++ source/funkin/editors/character/CharacterAnimsWindow.hx
@@ -1,12 +1,13 @@
 package funkin.editors.character;
 
+import flixel.graphics.frames.FlxFrame;
 import funkin.game.Character;
 
 class CharacterAnimsWindow extends UIButtonList<CharacterAnimButton> {
 	public var character:Character;
 
 	public var displayWindowSprite:FlxSprite;
-	public var displayAnimsList:Array<String> = [];
+	public var displayAnimsFramesList:Map<String, Int> = [];
 
 	public function new(x:Float, y:Float, character:Character) {
 		super(x, y, Std.int(500-16), 419, "", FlxPoint.get(Std.int(500-16-32), 184));
@@ -16,8 +17,12 @@ class CharacterAnimsWindow extends UIButtonList<CharacterAnimButton> {
 
 		displayWindowSprite = new FlxSprite();
 		displayWindowSprite.loadGraphicFromSprite(character);
-		
-		displayAnimsList = displayWindowSprite.animation.getNameList();
+		displayWindowSprite.antialiasing = character.antialiasing;
+		displayWindowSprite.flipX = character.flipX;
+
+		@:privateAccess
+		for (name => anim in displayWindowSprite.animation._animations) 
+			displayAnimsFramesList.set(name, anim.frames.getDefault([0])[0]);
 
 		for (anim in character.getAnimOrder())
 			add(new CharacterAnimButton(0,0, character.animDatas.get(anim), this));

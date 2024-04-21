c0b9a97f401c67630177bb1ba0c904b9f1972cfc
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-04-22 00:29:41 +0300

    Update ExternalMac.hx

--- source/external/ExternalMac.hx
+++ source/external/ExternalMac.hx
@@ -2,10 +2,11 @@ package external;
 
 #if mac
 @:build(external.LinkerMacro.xml('external_code.xml'))
-@:native("ExternalMac")
 @:include('Mac.h')
-extern class ExternalMac {
-	@:native("ExternalMac::setCursorIcon")
-	public static function setCursorIcon(icon:Int):Bool;
+@:native('ExternalMac')
+extern class ExternalMac
+{
+	@:native('ExternalMac::setCursorIcon')
+	public static function setCursorIcon(icon:Int, customCursor:cpp.ConstCharStar, customX:Single, customY:Single):Bool;
 }
 #end

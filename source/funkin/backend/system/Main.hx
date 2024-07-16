722af94ac858aaebe39333b5ee9072eb08a88e45
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-07-16 13:21:03 -0700

    some stuffers

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -35,7 +35,7 @@ class Main extends Sprite
 	public static var instance:Main;
 
 	public static var modToLoad:String = null;
-	public static var forceGPUOnlyBitmapsOff:Bool = #if windows false #else true #end;
+	public static var forceGPUOnlyBitmapsOff:Bool = #if desktop false #else true #end;
 	public static var noTerminalColor:Bool = false;
 
 	public static var scaleMode:FunkinRatioScaleMode;

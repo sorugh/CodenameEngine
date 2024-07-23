36f77a3d65d776889db1aa12d833e57eff63dd8e
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-07-23 14:29:49 -0700

    for windows peeps

--- source/funkin/editors/character/CharacterEditor.hx
+++ source/funkin/editors/character/CharacterEditor.hx
@@ -356,7 +356,7 @@ class CharacterEditor extends UIState {
 
 	inline function handleMouseOffsets() {
 		if (draggingCharacter) {
-			cameraHoverDummy.cursor = #if (mac) DRAG; #elseif (linux) DRAG; #else MOVE; #end
+			cameraHoverDummy.cursor = #if (mac) DRAG; #elseif (linux) DRAG; #else BUTTON; #end
 
 			if (FlxG.mouse.justReleased) {
 				draggingOffset.x /= character.scale.x;

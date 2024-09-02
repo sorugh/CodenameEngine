aa22bd6e1db79fe67d893135353e156036184d78
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2024-09-02 23:47:11 +0200

    fixing stuff

--- source/funkin/backend/system/framerate/FramerateCounter.hx
+++ source/funkin/backend/system/framerate/FramerateCounter.hx
@@ -1,27 +1,16 @@
 package funkin.backend.system.framerate;
 
-import haxe.ds.Vector;
 import openfl.display.Sprite;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 
-inline final FRAME_TIME_HISTORY = 20;
-
 class FramerateCounter extends Sprite {
 	public var fpsNum:TextField;
 	public var fpsLabel:TextField;
-
-	public var fpsHistoryIndex:Int = 0;
-	public var fpsHistory:Vector<Float>;
+	public var lastFPS:Float = 0;
 
 	public function new() {
 		super();
-
-		fpsHistory = new Vector(FRAME_TIME_HISTORY);
-		// Initialize to 60 FPS, so that the initial estimation until we get enough data is always reasonable.
-		for(i in 0...FRAME_TIME_HISTORY) {
-			fpsHistory[i] = 1000.0 / 60.0;
-		}
 
 		fpsNum = new TextField();
 		fpsLabel = new TextField();
@@ -43,20 +32,8 @@ class FramerateCounter extends Sprite {
 		if (alpha <= 0.05) return;
 		super.__enterFrame(t);
 
-		// todo use gpu time
-		fpsHistory[fpsHistoryIndex] = FlxG.elapsed * 1000;
-		fpsHistoryIndex = (fpsHistoryIndex + 1) % FRAME_TIME_HISTORY;
-
-		// Calculate average CPU time.
-		// Code based on Godot's FPS counter.
-		var cpuTime = 0.0;
-		for(i in 0...FRAME_TIME_HISTORY) {
-			cpuTime += fpsHistory[i];
-		}
-		cpuTime /= FRAME_TIME_HISTORY;
-		cpuTime = Math.max(0.01, cpuTime); // Prevent unrealistically low values.
-
-		fpsNum.text = Std.string(Math.floor(1000.0 / cpuTime));
+		lastFPS = CoolUtil.fpsLerp(lastFPS, FlxG.elapsed == 0 ? 0 : (1 / FlxG.elapsed), 0.25);
+		fpsNum.text = Std.string(Math.floor(lastFPS));
 		fpsLabel.x = fpsNum.x + fpsNum.width;
 		fpsLabel.y = (fpsNum.y + fpsNum.height) - fpsLabel.height;
 	}

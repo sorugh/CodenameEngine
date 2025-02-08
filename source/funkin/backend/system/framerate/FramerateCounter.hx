0a1517df6bb5374ab64f1149df856ae8e4d797c7
Ne_Eo <NeeEoo@users.noreply.github.com>
2025-02-08 01:24:55 +0100

    Fix framerate counter when using timescale

--- source/funkin/backend/system/framerate/FramerateCounter.hx
+++ source/funkin/backend/system/framerate/FramerateCounter.hx
@@ -33,7 +33,7 @@ class FramerateCounter extends Sprite {
 		if (alpha <= 0.05) return;
 		super.__enterFrame(t);
 
-		lastFPS = CoolUtil.fpsLerp(lastFPS, FlxG.elapsed == 0 ? 0 : (1 / FlxG.elapsed), 0.25);
+		lastFPS = CoolUtil.fpsLerp(lastFPS, FlxG.rawElapsed == 0 ? 0 : (1 / FlxG.rawElapsed), 0.25);
 		fpsNum.text = Std.string(Math.floor(lastFPS));
 		fpsLabel.x = fpsNum.x + fpsNum.width;
 		fpsLabel.y = (fpsNum.y + fpsNum.height) - fpsLabel.height;

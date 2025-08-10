4573fef74d76f70341702e7413df0d319ba10587
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-08-10 08:28:31 +0700

    Remove this trace

--- source/funkin/editors/charter/SongCreationScreen.hx
+++ source/funkin/editors/charter/SongCreationScreen.hx
@@ -465,7 +465,6 @@ class SongCreationScreen extends UISubstateWindow {
 			playerVocals: files.get('Voices-${playData.characters.playerVocals != null ? playData.characters.playerVocals[0] : playData.characters.player}.${Flags.SOUND_EXT}'),
 			oppVocals: files.get('Voices-${playData.characters.opponentVocals != null ? playData.characters.opponentVocals[0] : playData.characters.opponent}.${Flags.SOUND_EXT}'),
 		}, (songFolder:String) -> {
-			trace(songFolder);
 			#if sys
 			for (diff in diffCharts) CoolUtil.safeSaveFile('$songFolder/${getChartSavePath(meta, diff.diffName)}', Json.stringify(diff.chart, Flags.JSON_PRETTY_PRINT));
 			if (events != null) CoolUtil.safeSaveFile('$songFolder/events${meta.variant != null && meta.variant != "" ? "-" + meta.variant : ""}.json', Json.stringify({events: events}, Flags.JSON_PRETTY_PRINT));
